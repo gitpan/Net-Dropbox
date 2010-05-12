8ec52b2e61dc4fbaf04e60f462023c44aaa6bdb8
Lenz Gschwendtner <norbu@cpan.org>
2010-05-12 02:15:18 +0000

    Import of NORBU/Net-Dropbox-0.01 from CPAN.
    gitpan-cpan-distribution: Net-Dropbox
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         NORBU/Net-Dropbox-0.01.tar.gz
    gitpan-cpan-author:       NORBU
    gitpan-cpan-maturity:     released

--- lib/Net/Dropbox.pm
+++ lib/Net/Dropbox.pm
@@ -1,317 +1,269 @@
 package Net::Dropbox;
-our $VERSION = '1.091510';
 
-# ABSTRACT: Communicate with local Dropbox daemon
-
-use warnings;
-use strict;
-
-use File::HomeDir;
-
-use Moose;
-use Moose::Util::TypeConstraints;
-
-use MooseX::StrictConstructor;
-
-use IO::Socket;
-use IO::Socket::UNIX;
-
-use Encode;
-
-subtype SocketExists 
-	=> as 'Str'
-	=> where { -S $_ }
-	=> message { "The socket name you provided, $_, is not a valid socket" };
-
-has 'command_socket' => ( 
-	is      => 'ro', 
-	isa     => 'SocketExists', 
-	default => File::HomeDir->my_home.'/.dropbox/command_socket' 
-);
-
-has 'hold_connection_open' => ( 
-	is      => 'ro', 
-	isa     => 'Bool', 
-	default => 0 
-);
-
-# This is hardcoded into the official C client so I'm mirroring it
-has 'max_args' => (
-	is         => 'ro',
-	isa        => 'Int',
-	default    => 20,
-	init_arg   => undef
-);
-
-has '_socket' => (
-	is        => 'rw',
-	isa       => 'FileHandle',
-	init_arg  => undef
-);
-
-
-
-sub file_status { #{{{
-	my $self = shift;
-	my $file = shift;
-
-	my $res = $self->_send_and_fetch(
-		"icon_overlay_file_status",
-		{ path => $file },
-	);
-
-	return $res;
-} #}}}
-
-
-
-
-
-sub dir_status { #{{{
-	my $self = shift;
-	my $dir  = shift;
-
-	return { ok => 0, status => 'not a dir' } 
-		unless( -d $dir );
-
-	my $res = $self->_send_and_fetch(
-		"icon_overlay_file_status",
-		{ path => $dir },
-	);
-
-	if($res->{ok} == 1) {
-		my $res2 = $self->_send_and_fetch(
-			"get_folder_tag",
-			{ path => $dir },
-		);
-
-		if($res2->{ok} == 1) {
-			for my $key ( keys %$res2 ) {
-				$res->{$key} = $res2->{$key};
-			}
-			
-		} else {
-			return $res2;
-		}
-	}
-
-	return $res;
-
-} #}}}
-
-
-######################
-# Internal methods
-######################
-
-
-sub _send_and_fetch { #{{{
-	my $self = shift;
-	my $cmd = shift;
-	my $args = shift;
-
-	my $cmdtxt = $self->_build_command($cmd, $args);
-
-	$self->_open_connection();
-	my $sock = $self->_socket();
-	print $sock $cmdtxt or die "print : $!\n";
-
-	my $buf = $self->_read_sock_buffer();
-	$self->_close_connection();
-	
-	my $res = $self->_parse_response($buf);
-
-	return $res;
-} #}}}
-
-sub _build_command { #{{{
-	my $self    = shift;
-	my $command = shift;
-	my $args    = shift;
-
-	my $output = "$command\n";
-	foreach my $arg (sort keys %$args) {
-		$output .= "$arg\t$args->{$arg}\n";
-	}
-	$output .= "done\n";
-
-	$output = encode("utf8", $output);
-
-	return $output;
-} #}}}
-
-sub _parse_response { #{{{
-	my $self = shift;
-	my $text = shift;
-
-	my $res = { ok => 0 };
-	my @lines = split("\n", $text);
-
-	#use Data::Dumper; print Dumper \@lines;
-
-	pop @lines; # remove the 'done' on the end
-
-	my $type = shift @lines;
-
-	if($type eq 'notok') {
-		$res->{ok} = 0;
-		$res->{reason} = shift @lines;
-
-	} elsif ($type eq 'ok') {
-		$res->{ok} = 1;
-
-		foreach my $line (@lines) {
-			if($line =~ /^(.+?)\t$/) {
-				# single word response. Mark it as defined but empty
-				$res->{$1} = '';
-				next;
-			}
-
-			my ($key, $value) = $line =~ /^(.+?)\t(.+?)$/;
-
-			if($key eq 'options') {
-				$res->{options} = {};
-
-				my @bits = split(/\t/, $value);
-				foreach my $bit (@bits) {
-					my ($short, $long, $action) = split(/~/, $bit);
-					$res->{options}->{$action} = {
-						short => $short,
-						long => $long,
-						action => $action,
-					};
-				}
-			} else {
-				$res->{$key} = $value;
-			}
-		}
-	}
-
-	
-	return $res;
-} #}}}
-
-
-sub _open_connection { #{{{
-	my $self = shift;
-	my $sock = IO::Socket::UNIX->new($self->command_socket) 
-		or die $!;
-	
-	$sock->autoflush(1);
-
-	$self->_socket($sock);
-} #}}}
-
-sub _close_connection { #{{{
-	my $self = shift;
-
-	unless($self->hold_connection_open) {
-		close $self->_socket();
-	}
-} #}}}
-
-sub _read_sock_buffer { #{{{
-	my $self = shift;
-	my $sock = $self->_socket();;
-
-	my ($line, $buf);
-	while (defined( $line = <$sock> )) {
-		$buf .= $line;
-		chomp $line;
-		if ($line eq 'done') {
-			last;
-		}
-	}
-	return $buf;
-} #}}}
-
-1;
-
-
-
-=pod
+use common::sense;
+use JSON;
+use Mouse;
+use Net::OAuth;
+use LWP::UserAgent;
+use HTTP::Request::Common;
+use Data::Random qw(rand_chars);
 
 =head1 NAME
 
-Net::Dropbox - Communicate with local Dropbox daemon
+Net::Dropbox - The great new Net::Dropbox!
 
 =head1 VERSION
 
-version 1.091510
+Version 0.01
 
-=head1 THANKS
+=cut
 
-This module wouldn't exist without the Dropbox community. With the lack of
-documentation and very confusing and poorly written official libraries,
-these fine folks did most of my work for me. I think I've read through every
-piece of code on the wiki (L<http://wiki.getdropbox.com>) in
-my attempt to figure out this protocol.  
-
-Particularly, I want to point out Filip Lundborg (filip@mkeyd.net). His
-dbcli.py and status.py files provided much knowledge and helped me more than
-any other piece of code I ran across. Thanks, Filip. I owe you a beverage of
-your choice. :)
+our $VERSION = '0.01';
 
 
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::Dropbox;
+
+    my $foo = Net::Dropbox->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=cut
+
+has 'debug' => (is => 'rw', isa => 'Bool', default => 0);
+has 'error' => (is => 'rw', isa => 'Str', predicate => 'has_error');
+has 'key' => (is => 'rw', isa => 'Str');
+has 'secret' => (is => 'rw', isa => 'Str');
+has 'nonce' => (is => 'ro', isa => 'Str', default => join( '', rand_chars( size => 16, set => 'alphanumeric' ) ));
+has 'login_link' => (is => 'rw', isa => 'Str');
+has 'callback_url' => (is => 'rw', isa => 'Str', default => 'http://localhost:3000/callback');
+has 'request_token' => (is => 'rw', isa => 'Str');
+has 'request_secret' => (is => 'rw', isa => 'Str');
+has 'access_token' => (is => 'rw', isa => 'Str');
+has 'access_secret' => (is => 'rw', isa => 'Str');
+
+=head2 login
+This sets up the initial OAuth handshake and returns the login URL. This
+URL has to be clicked by the user and the the user then has to accept
+the application in dropbox. 
+Dropbox then redirects back to the callback URL defined with
+$self->callback_url. If the user already accepted the application the
+redirect may happen without the user actually clicking anywhere.
+=cut
+
+sub login {
+    my $self = shift;
+
+    my $ua = LWP::UserAgent->new;
+
+    my $request = Net::OAuth->request("request token")->new(
+        consumer_key => $self->key,
+        consumer_secret => $self->secret,
+        request_url => 'http://api.dropbox.com/0/oauth/request_token',
+        request_method => 'POST',
+        signature_method => 'HMAC-SHA1',
+        timestamp => time,
+        nonce => $self->nonce,
+        callback => $self->callback_url,
+    );
+
+    $request->sign;
+    my $res = $ua->request(GET $request->to_url);
+
+    if ($res->is_success) {
+        my $response = Net::OAuth->response('request token')->from_post_body($res->content);
+        $self->request_token($response->token);
+        $self->request_secret($response->token_secret);
+        print "Got Request Token ", $response->token, "\n" if $self->debug;
+        print "Got Request Token Secret ", $response->token_secret, "\n" if $self->debug;
+        return 'http://api.dropbox.com/0/oauth/authorize?oauth_token='.$response->token.'&oauth_callback='.$self->callback_url;
+    }
+    else {
+        $self->error($res->status_line);
+        warn "Something went wrong: ".$res->status_line;
+    }
+}
+
+=head2 auth
+The auth method changes the initial request token into access token that we need
+for subsequent access to the API. This method only has to be called once
+after login.
+=cut
+
+sub auth {
+    my $self = shift;
+
+    my $ua = LWP::UserAgent->new;
+    my $request = Net::OAuth->request("access token")->new(
+        consumer_key => $self->key,
+        consumer_secret => $self->secret,
+        request_url => 'http://api.dropbox.com/0/oauth/access_token',
+        request_method => 'POST',
+        signature_method => 'HMAC-SHA1',
+        timestamp => time,
+        nonce => $self->nonce,
+        callback => $self->callback_url,
+        token => $self->request_token,
+        token_secret => $self->request_secret,
+    );
+
+    $request->sign;
+    my $res = $ua->request(GET $request->to_url);
+
+    if ($res->is_success) {
+        my $response = Net::OAuth->response('access token')->from_post_body($res->content);
+        $self->access_token($response->token);
+        $self->access_secret($response->token_secret);
+        print "Got Access Token ", $response->token, "\n" if $self->debug;
+        print "Got Access Token Secret ", $response->token_secret, "\n" if $self->debug;
+    }
+    else {
+        $self->error($res->status_line);
+        warn "Something went wrong: ".$res->status_line;
+    }
+}
+
+=head2 account_info
+account_info polls the users info from dropbox.
+=cut
+
+sub account_info {
+    my $self = shift;
+
+    return from_json($self->_talk('account/info'));
+}
+
+=head2 list_files
+lists all files un the path defined.
+Paths starting with 'dropbox/' refer to the users dropbox and paths
+starting with 'sandbox/' refer to the sandbox directory the user asigned
+to the application.
+=cut
+sub list_files {
+    my $self = shift;
+    my $path = shift;
+
+    return from_json($self->_talk('files/'.$path));
+}
 
 
-=head1 METHODS
 
-=head2 file_status
+=head1 INTERNAL API
+=head2 _talk
+_talk handles the access to the restricted resources. You should
+normally not need to access this directly.
+=cut
 
-	my $hash_ref = $self->file_status("file name");
+sub _talk {
+    my $self = shift;
+    my $command = shift;
+    my $method = shift || 'GET';
 
-Retrieves the status of the provided filename and returns it as a hash
-reference.
+    my $ua = LWP::UserAgent->new;
+    my $request = Net::OAuth->request("protected resource")->new(
+        consumer_key => $self->key,
+        consumer_secret => $self->secret,
+        request_url => 'http://api.dropbox.com/0/'.$command,
+        request_method => $method,
+        signature_method => 'HMAC-SHA1',
+        timestamp => time,
+        nonce => $self->nonce,
+        #callback => $self->callback_url,
+        token => $self->access_token,
+        token_secret => $self->access_secret,
+    );
 
-First and only argument is the file name to retrieve status for. This
-B<must> be a fully formed uri like C</home/sungo/Dropbox/test.txt> 
+    $request->sign;
 
-If the transaction is successful, 'ok' will be set to 1 and 'status'
-will be a string. 'up to date', 'syncing', and 'unsyncable' are
-possible values of the string. 
+    my $res;
+    if($method =~ /get/i){
+        $res = $ua->get($request->to_url);
+    } else {
+        $res = $ua->post($request->to_url);
+    }
 
-If the transaction is unsuccessful, 'ok' will be set to 0 and 'reason'
-will be set to the error string returned by the server. 
+    if ($res->is_success) {
+        print "Got Content ", $res->content, "\n" if $self->debug;
+        return $res->content;
+    }
+    else {
+        $self->error($res->status_line);
+        warn "Something went wrong: ".$res->status_line;
+    }
+    return;
+}
+=head2 talk
 
-
-
-=head2 dir_status 
-
-	my $hash_ref = $self->dir_status("directory name");
-
-Retrieves the status of the provided directory and returns it as a hash
-reference.
-
-The first and only argument is the directory name to retrieve status
-for. This B<must> be a fully formed uri like C</home/sungo/Dropbox/Dir> 
-
-If the transaction is successful, 'ok' will be set to 1. 'status'
-will be a string with the possible values of 'up to date', 'syncing',
-and 'unsyncable'. 'tag' will be a string with the possible values of
-'public', 'shared' and 'photos'. If there is no tags, 'tag' will be an
-empty string. 
-
-If the transaction is unsuccessful, 'ok' will be set to 0 and 'reason'
-will be set to the error string returned by the server. 
-
-
+=cut
 
 =head1 AUTHOR
 
-  sungo <sungo@sungo.us>
+Lenz Gschwendtner, C<< <norbu09 at cpan.org> >>
 
-=head1 COPYRIGHT AND LICENSE
+=head1 BUGS
 
-This software is Copyright (c) 2009 by Matt Cashner (sungo).
-
-This is free software, licensed under:
-
-  The (three-clause) BSD License
-
-=cut 
+Please report any bugs or feature requests to C<bug-net-dropbox at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-Dropbox>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
 
 
 
-__END__
 
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::Dropbox
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-Dropbox>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Net-Dropbox>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Net-Dropbox>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Net-Dropbox/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Lenz Gschwendtner.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Net::Dropbox
